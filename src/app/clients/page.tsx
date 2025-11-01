bc4d3d164e253da197b390bcc064245512be3a2b
xhenonpro <xhenonprototype@gmail.com>
2025-11-01 04:21:50 +0300

    Removed /clients folder

--- src/app/clients/page.tsx
+++
@@ -1,3 +0,0 @@
-<div className="pt-20 md:pl-72 p-6 md:p-10 bg-gradient-to-br from-gray-50 to-blue-100 dark:from-gray-950 dark:to-purple-950 dark:via-blue-950 min-h-screen text-gray-900 dark:text-white">
-  {/* ...existing code... */}
-</div>
